10074c2794869561d678cb7f07c31199aae32a45
Lucasdelapena <lucas.delapena@outlook.com>
2024-10-31 10:09:34 -0500

    updated code

--- NumberGen.py
+++ NumberGen.py
@@ -1,13 +1,25 @@
 import numpy as np
 import random
 
+def generateNumbers(num, tvs):
+    if num % tvs == 0: #if the number is evenly divisible
+        playerPerTv = num // tvs
+
+    else:
+        playerPerTv = num // tvs + 1
+
+    return playerPerTv
+
 def main():
     #Number of players
     num = int(input("How many players? "))
     #Number of tvs
     tvs = int(input("How many tvs are you using? "))
     #Number of numbers per tv
-    playerPerTv = int(input("How many players per tv? "))
+    #playerPerTv = int(input("How many players per tv? "))
+   
+    # Calculate Players per TV
+    playerPerTv = generateNumbers(num, tvs)
 
     #players array 
     players = np.arange(1, num+1)
@@ -15,7 +27,18 @@ def main():
     #Generate the numbers
     random.shuffle(players)
 
+    for i in range(tvs):
+        print(f"TV {i + 1}:")
+        for j in range(playerPerTv):
+            if len(players) == 0:
+                break
+            print(f"  Player {players[0]}")
+            players = np.delete(players, 0)
+        print()  # Print an empty line for separation
 
+
+
+"""""
     while len(players) > 0:
         for i in range(tvs):
             for j in range(playerPerTv):
@@ -30,6 +53,7 @@ def main():
 
             players = np.delete(players, range(playerPerTv)) # Remove the players that have been printed
             print()
+"""""
         
 
 if __name__ == "__main__":
