75eb3cb2cc530f6a7accf95ae3bc40e21c2ce079
Lucasdelapena <lucas.delapena@outlook.com>
2024-10-31 13:47:24 -0500

    finished making time to make visuals

--- NumberGen.py
+++ NumberGen.py
@@ -1,25 +1,37 @@
 import numpy as np
 import random
 
-def generateNumbers(num, tvs):
-    if num % tvs == 0: #if the number is evenly divisible
-        playerPerTv = num // tvs
+def generateNumbers(num):
+    oneRemaining, twoRemaining, threeRemaining = False, False, False
+    
+    if num % 4 == 0: #if the number is evenly divisible
+        playerPerTv = 4
 
+    elif num % 3 == 0: #3 playres per tv
+        playerPerTv = 3
+    
     else:
-        playerPerTv = num // tvs + 1
+        playerPerTv = 4
+        if num % 4 == 1:
+            #last 3 tvs will have 3 players
+            oneRemaining = True
+        elif num % 4 == 2:
+            #last 2 tvs will have 3 players
+            twoRemaining = True
+        elif num % 4 == 3:
+            #last tv will have 3 players
+            threeRemaining = True
 
-    return playerPerTv
+    return playerPerTv, oneRemaining, twoRemaining, threeRemaining
 
 def main():
     #Number of players
     num = int(input("How many players? "))
     #Number of tvs
     tvs = int(input("How many tvs are you using? "))
-    #Number of numbers per tv
-    #playerPerTv = int(input("How many players per tv? "))
-   
+
     # Calculate Players per TV
-    playerPerTv = generateNumbers(num, tvs)
+    playerPerTv, oneRemaining, twoRemaining, threeRemaining = generateNumbers(num)
 
     #players array 
     players = np.arange(1, num+1)
@@ -27,33 +39,20 @@ def main():
     #Generate the numbers
     random.shuffle(players)
 
-    for i in range(tvs):
-        print(f"TV {i + 1}:")
-        for j in range(playerPerTv):
-            if len(players) == 0:
-                break
-            print(f"  Player {players[0]}")
-            players = np.delete(players, 0)
-        print()  # Print an empty line for separation
-
-
-
-"""""
     while len(players) > 0:
         for i in range(tvs):
             for j in range(playerPerTv):
-
                 if len(players) == 0:
                     break
-
                 print("TV:", i+1, " Player: ", players[j]) #Print the player number
-
             if len(players) == 0:
                 break
-
             players = np.delete(players, range(playerPerTv)) # Remove the players that have been printed
+            if oneRemaining and len(players) == 9 or twoRemaining and len(players) == 6 or threeRemaining and len(players) == 3:
+                #dividing the remaining players by 3
+                playerPerTv = 3
             print()
-"""""
+
         
 
 if __name__ == "__main__":
---
+++ testing.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def main():
+    print(19 % 4) # 3 
+    print(19 % 3) # 1
+    print(19 % 2) # 1
+    print(19 // 4) # 4
+
+    print(26 % 4)
+
+
+    print (16 % 5) # 1
+
+    print (18 % 4) # 2
+    print (18 % 3) # 0
+    print (18 % 2) # 0
+
+    print (17 % 4) # 1
+    print (17 % 3) # 2
+    print (17 % 2) # 1
+
+    
+
+
+if __name__ == "__main__":
+    main()
