65d3d858445ff309f5123ebd0dd29ea2d0ae38e0
Lucasdelapena <lucas.delapena@outlook.com>
2024-11-08 20:56:53 -0600

    progress

--- NumberGen.py
+++ NumberGen.py
@@ -6,15 +6,15 @@ from ttkbootstrap.constants import *
 
 def updateWindow(nums, tvs):
     # Clear the frame
-    for widget in frame.winfo_children():
+    for widget in secondFrame.winfo_children():
         widget.destroy()
-
-    ttk.Label(frame, text="Placements", style='warning.TLabel', font=("Helvetica", 30, "bold")).pack(side = 'top', pady=30 )
-    ttk.Label(frame, text="Number of Players: " + str(nums), style='TLabel').pack(pady=0)
-    ttk.Label(frame, text="Number of TVs: " + str(tvs), style='TLabel').pack(pady=0)
+    outerFrame.geometry("500x700")
+    #ttk.Scrollbar(secondFrame, orient='vertical').pack(side='right', fill='y')
+    ttk.Label(secondFrame, text="Placements", style='warning.TLabel', font=("Helvetica", 30, "bold")).pack(side = 'top', pady=30 )
+    ttk.Label(secondFrame, text="Number of Players: " + str(nums), style='TLabel').pack(pady=0)
+    ttk.Label(secondFrame, text="Number of TVs: " + str(tvs), style='TLabel').pack(pady=0)
     placements(nums, tvs)
-
-    
+ 
 def onSubmit():
     num = int(inputnum.get())
     tvs = int(inputtvs.get())
@@ -22,35 +22,33 @@ def onSubmit():
 
 def reset():
     # Clear the frame
-    for widget in frame.winfo_children():
+    for widget in secondFrame.winfo_children():
         widget.destroy()
     
     # Recreate the initial labels and input fields
-    ttk.Label(frame, text="Mario Kart Tournament Generator", style='warning.TLabel', font=("Helvetica", 16, "bold")).pack(pady=40, anchor='center')
+    ttk.Label(secondFrame, text="Mario Kart Tournament Generator", style='warning.TLabel', font=("Helvetica", 16, "bold")).pack(pady=40, anchor='center')
 
-    ttk.Label(frame, text="Number of Players", style='info.TLabel').pack(pady=10, anchor='center')
+    ttk.Label(secondFrame, text="Number of Players", style='info.TLabel').pack(pady=10, anchor='center')
     global inputnum  # Declare as global to modify outside of function
-    inputnum = ttk.Spinbox(frame, from_=1, to=100, style='info.TSpinbox')
+    inputnum = ttk.Spinbox(secondFrame, from_=1, to=100, style='info.TSpinbox')
     inputnum.pack(pady=10, anchor='center')
 
-    ttk.Label(frame, text="Number of TVs", style='info.TLabel').pack(pady=10, anchor='center')
+    ttk.Label(secondFrame, text="Number of TVs", style='info.TLabel').pack(pady=10, anchor='center')
     global inputtvs  # Declare as global to modify outside of function
-    inputtvs = ttk.Spinbox(frame, from_=1, to=100, style='info.TSpinbox')
+    inputtvs = ttk.Spinbox(secondFrame, from_=1, to=100, style='info.TSpinbox')
     inputtvs.pack(pady=10, anchor='center')
 
     # Button
     global b  # Declare as global to modify outside of function
-    b = ttk.Button(frame, text='Submit', style='info.TButton', command=onSubmit)
+    b = ttk.Button(secondFrame, text='Submit', style='info.TButton', command=onSubmit)
     b.pack(padx=5, pady=10, anchor='center')
 
-
 def showAbout():
-    aboutWindow = Toplevel(root)
+    aboutWindow = Toplevel(outerFrame)
     aboutWindow.title("About")
     aboutWindow.geometry("300x200")
     about_label = ttk.Label(aboutWindow, text="This is the Mario Kart Tournament Generator.\nVersion 1.0\nDeveloped by Your Name", font=("Helvetica", 12))
-    about_label.pack(pady=50)
-                           
+    about_label.pack(pady=50)                         
 
 def generateNumbers(num):
     oneRemaining, twoRemaining, threeRemaining = False, False, False
@@ -73,30 +71,31 @@ def generateNumbers(num):
     return playerPerTv, oneRemaining, twoRemaining, threeRemaining
 
 def threePerTv(players, tvs, nums):
-    for widget in frame.winfo_children():
+    for widget in secondFrame.winfo_children():
         widget.destroy()
-    ttk.Label(frame, text="Placements", style='warning.TLabel', font=("Helvetica", 30, "bold")).pack(side = 'top', pady=30 )
-    ttk.Label(frame, text="Number of Players: " + str(nums), style='TLabel').pack(pady=0)
-    ttk.Label(frame, text="Number of TVs: " + str(tvs), style='TLabel').pack(pady=0)
+
+    ttk.Label(secondFrame, text="Placements", style='warning.TLabel', font=("Helvetica", 30, "bold")).pack(side = 'top', pady=30 )
+    ttk.Label(secondFrame, text="Number of Players: " + str(nums), style='TLabel').pack(pady=0)
+    ttk.Label(secondFrame, text="Number of TVs: " + str(tvs), style='TLabel').pack(pady=0)
     round = 0
     playerPerTv = 3
     while len(players) > 0: #works with 7 and up
         round += 1
         for i in range(tvs):
             if i == 0 and len(players) != 0:
-                ttk.Label(frame, text="Round " + str(round), style='danger.TLabel', font=("Helvetica", 18, "bold")).pack(pady=10)
+                ttk.Label(secondFrame, text="Round " + str(round), style='danger.TLabel', font=("Helvetica", 18, "bold")).pack(pady=10)
             if len(players) != 0:
-                ttk.Label(frame, text="TV "+ str(i+1), style='TLabel', font=("Helvetica", 12, "bold")).pack(pady=10)
+                ttk.Label(secondFrame, text="TV "+ str(i+1), style='TLabel', font=("Helvetica", 12, "bold")).pack(pady=10)
             for j in range(playerPerTv):
                 if len(players) == 0:
                     break
-                ttk.Label(frame, text= "Player " + str(players[j]), style='info.TLabel').pack(pady=0)
+                ttk.Label(secondFrame, text= "Player " + str(players[j]), style='info.TLabel').pack(pady=0)
             if len(players) == 0:
                 break
             players = np.delete(players, range(playerPerTv)) # Remove the players that have been printed
             print()
 
-    ttk.Label(frame, text="", style='success.TLabel', font=("Helvetica", 18, "bold")).pack(pady=50)
+    ttk.Label(secondFrame, text="", style='success.TLabel', font=("Helvetica", 18, "bold")).pack(pady=50)
     
 def placements(num, tvs):
     # Calculate Players per TV
@@ -111,45 +110,45 @@ def placements(num, tvs):
     #ttk.Label(frame, text="" + str(tvs), style='TLabel').pack(pady=0)
     
     if num % 3 == 0:
-        ttk.Label(frame, text="This is divisible by 3.", style='danger.TLabel', font="bo").pack(pady=(20,0))
-        ttk.Label(frame, text="Did you want to have 3 players per tv?", style='TLabel').pack(pady=0)
+        ttk.Label(secondFrame, text="This is divisible by 3.", style='danger.TLabel', font="bo").pack(pady=(20,0))
+        ttk.Label(secondFrame, text="Did you want to have 3 players per tv?", style='TLabel').pack(pady=0)
         players2 = players
-        three = ttk.Button(frame, text='3 per tv', style='info.TButton', command=lambda: threePerTv(players2, tvs, num))
+        three = ttk.Button(secondFrame, text='3 per tv', style='info.TButton', command=lambda: threePerTv(players2, tvs, num))
         three.pack(padx=5, pady=10, anchor='center')
 
     if len(players) <= 4:
 
-        ttk.Label(frame, text="Round 1", style='danger.TLabel', font=("Helvetica", 18, "bold")).pack(pady=10)
-        ttk.Label(frame, text="TV 1", style='TLabel', font=("Helvetica", 12, "bold")).pack(pady=10)
+        ttk.Label(secondFrame, text="Round 1", style='danger.TLabel', font=("Helvetica", 18, "bold")).pack(pady=10)
+        ttk.Label(secondFrame, text="TV 1", style='TLabel', font=("Helvetica", 12, "bold")).pack(pady=10)
         length = len(players)
         for j in range(length):
-            ttk.Label(frame, text= "Player " + str(players[j]), style='info.TLabel').pack(pady=0)
+            ttk.Label(secondFrame, text= "Player " + str(players[j]), style='info.TLabel').pack(pady=0)
         players = np.delete(players,length) # Remove the players that have been printed
 
     if len(players) == 5:
-        ttk.Label(frame, text="Round 1", style='danger.TLabel', font=("Helvetica", 18, "bold")).pack(pady=10)
-        ttk.Label(frame, text="TV 1", style='TLabel', font=("Helvetica", 12, "bold")).pack(pady=10)
+        ttk.Label(secondFrame, text="Round 1", style='danger.TLabel', font=("Helvetica", 18, "bold")).pack(pady=10)
+        ttk.Label(secondFrame, text="TV 1", style='TLabel', font=("Helvetica", 12, "bold")).pack(pady=10)
         for j in range(3):
-            ttk.Label(frame, text= "Player " + str(players[j]), style='info.TLabel').pack(pady=0)
+            ttk.Label(secondFrame, text= "Player " + str(players[j]), style='info.TLabel').pack(pady=0)
         players = np.delete(players,range(3)) # Remove the players that have been printed
         print()
-        ttk.Label(frame, text="Round 2", style='danger.TLabel', font=("Helvetica", 18, "bold")).pack(pady=10)
-        ttk.Label(frame, text="TV 2", style='TLabel', font=("Helvetica", 12, "bold")).pack(pady=10)
+        ttk.Label(secondFrame, text="Round 2", style='danger.TLabel', font=("Helvetica", 18, "bold")).pack(pady=10)
+        ttk.Label(secondFrame, text="TV 2", style='TLabel', font=("Helvetica", 12, "bold")).pack(pady=10)
         for j in range(2):
-            ttk.Label(frame, text= "Player " + str(players[j]), style='info.TLabel').pack(pady=0)
+            ttk.Label(secondFrame, text= "Player " + str(players[j]), style='info.TLabel').pack(pady=0)
         players = np.delete(players,range(2)) # Remove the players that have been printed
 
     while len(players) > 0: #works with 7 and up
         round += 1
         for i in range(tvs):
             if i == 0 and len(players) != 0:
-                ttk.Label(frame, text="Round " + str(round), style='danger.TLabel', font=("Helvetica", 18, "bold")).pack(pady=10)
+                ttk.Label(secondFrame, text="Round " + str(round), style='danger.TLabel', font=("Helvetica", 18, "bold")).pack(pady=10)
             if len(players) != 0:
-                ttk.Label(frame, text="TV "+ str(i+1), style='TLabel', font=("Helvetica", 12, "bold")).pack(pady=10)
+                ttk.Label(secondFrame, text="TV "+ str(i+1), style='TLabel', font=("Helvetica", 12, "bold")).pack(pady=10)
             for j in range(playerPerTv):
                 if len(players) == 0:
                     break
-                ttk.Label(frame, text= "Player " + str(players[j]), style='info.TLabel').pack(pady=0)
+                ttk.Label(secondFrame, text= "Player " + str(players[j]), style='info.TLabel').pack(pady=0)
             if len(players) == 0:
                 break
             players = np.delete(players, range(playerPerTv)) # Remove the players that have been printed
@@ -158,70 +157,60 @@ def placements(num, tvs):
                 playerPerTv = 3
             print()
 
-    ttk.Label(frame, text="", style='success.TLabel', font=("Helvetica", 18, "bold")).pack(pady=50)
-
-def _on_mouse_wheel(event):
-    frame.yview_scroll(int(-1*(event.delta/120)), "units")
+    ttk.Label(secondFrame, text="", style='success.TLabel', font=("Helvetica", 18, "bold")).pack(pady=50)
 
 
-#Window
-root = ttk.Window(themename="darkly")
-root.title("Mario Kart Tournament Generator")
+
+
+# Window
+outerFrame = ttk.Window(themename="darkly")
+outerFrame.title("Mario Kart Tournament Generator")
 
 # Size
-root.geometry("500x700") #make the window smaller
+outerFrame.geometry("500x450") #make the window smaller
 
-frame = ttk.Frame(root)
-frame.pack(expand = True, fill='both')
+# Canvas
+myCanvas = Canvas(outerFrame)
+myCanvas.pack(side=LEFT, fill=BOTH, expand=True)
 
-frame.bind_all("<MouseWheel>", _on_mouse_wheel)
+# scrollbar
+myScrollbar = ttk.Scrollbar(outerFrame, orient=VERTICAL, command=myCanvas.yview)
+myScrollbar.pack(side=RIGHT, fill=Y)
 
+# configure the canvas
+myCanvas.configure(yscrollcommand=myScrollbar.set)
+myCanvas.bind('<Configure>', lambda e: myCanvas.configure(scrollregion = myCanvas.bbox("all")))
 
-# Canvas and scrollbar
-#canvas = ttk.Canvas(root)
-#scrollbar = ttk.Scrollbar(root, orient="vertical", command=canvas.yview)
-#scrollbar.pack(side="right", fill="y")
-#canvas.pack(side="left", fill="both", expand=True)
-
-# Frame inside the canvas
-#frame = ttk.Frame(canvas)
-#canvas.create_window((0, 0), window=frame, anchor="n")
-
-# Configure the canvas to scroll with the scrollbar
-#frame.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
-#canvas.configure(yscrollcommand=scrollbar.set)
-
-# Mouse wheel
-#canvas.bind_all("<MouseWheel>", lambda e: canvas.yview_scroll(int(-1 * (e.delta / 120)), "units"))
-
+# Second Frame
+secondFrame = ttk.Frame(myCanvas)
+myCanvas.create_window((0,0), window=secondFrame, anchor="nw")
 
 
 # Menu
-menu = Menu(root)
-root.config(menu=menu)
+menu = Menu(outerFrame)
+outerFrame.config(menu=menu)
 filemenu = Menu(menu)
 menu.add_cascade(label='File', menu=filemenu)
 filemenu.add_command(label='Reset', command=reset)
 filemenu.add_separator()
-filemenu.add_command(label='Exit', command=root.quit)
+filemenu.add_command(label='Exit', command=outerFrame.quit)
 helpmenu = Menu(menu)
 menu.add_cascade(label='Help', menu=helpmenu)
 helpmenu.add_command(label=showAbout)
 
-
 # Labels
-ttk.Label(frame, text="Mario Kart Tournament Generator", style='warning.TLabel', font=("Helvetica", 16, "bold")).pack(pady=40, anchor='center')
+ttk.Label(secondFrame, text="Mario Kart Tournament Generator", style='warning.TLabel', font=("Helvetica", 16, "bold")).pack(pady=40, anchor='center')
 
-ttk.Label(frame, text="Number of Players", style='info.TLabel').pack(pady=10, anchor='center')
-inputnum = ttk.Spinbox(frame, from_=1, to=100, style='info.TSpinbox')
+ttk.Label(secondFrame, text="Number of Players", style='info.TLabel').pack(pady=10, anchor='center')
+inputnum = ttk.Spinbox(secondFrame, from_=1, to=100, style='info.TSpinbox')
 inputnum.pack(pady=10, anchor='center')
 
-ttk.Label(frame, text="Number of TVs", style='info.TLabel').pack(pady=10, anchor='center')
-inputtvs = ttk.Spinbox(frame, from_=1, to=100, style='info.TSpinbox')
+ttk.Label(secondFrame, text="Number of TVs", style='info.TLabel').pack(pady=10, anchor='center')
+inputtvs = ttk.Spinbox(secondFrame, from_=1, to=100, style='info.TSpinbox')
 inputtvs.pack(pady=10, anchor='center')
 
 # Button
-b = ttk.Button(frame, text='Submit', style='info.TButton', command=onSubmit)
+b = ttk.Button(secondFrame, text='Submit', style='info.TButton', command=onSubmit)
 b.pack(padx=5, pady=10, anchor='center')
 
-root.mainloop()
+outerFrame.mainloop()
